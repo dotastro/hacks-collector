40cf2433247dade3ea0aef53baa708c144c8bb07
Erik Tollerud <erik.tollerud@gmail.com>
2016-06-22 18:38:01 +0100

    inprogress update

--- submission-app/submission/webapp.py
+++ submission-app/submission/webapp.py
@@ -1,21 +1,21 @@
 import os
 import uuid
 import requests
+from urllib.parse import urlencode, parse_qs
 
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, redirect
 from github import Github
 
 GITHUB_AUTH_URL = 'https://github.com/login/oauth/authorize'
+GITHUB_TOKEN_URL = 'https://github.com/login/oauth/access_token'
+
+HACKLIST_REPO = 'dotastro/hack-list'
+
 CLIENT_ID = os.environ['CLIENT_ID']
 CLIENT_SECRET = os.environ['CLIENT_SECRET']
 
 app = Flask(__name__)
 
-def github_authorize():
-    data = {'client_id': CLIENT_ID,
-            'client_secret': CLIENT_SECRET,
-            'state': uuid.uuid4()}
-    res = requests.get(GITHUB_AUTH_URL, data=data)
 
 @app.route("/")
 def index():
@@ -25,24 +25,25 @@ def index():
 def assets(filename):
     return send_from_directory('form/assets', filename)
 
-# @app.route("/form/<path:filename>")
-# def getstatic(filename):
-#     return send_from_directory('form', filename)
 
-@app.route("/create", methods=['POST', 'GET'])
-def create():
-    token = github_authorize()
+@app.route("/create", methods=['POST'])
+def github_authorize():
+    data = {'client_id': CLIENT_ID,
+            'redirect_uri': request.base_url + '?' + urlencode(request.form)}
+    pr = requests.Request('GET', GITHUB_AUTH_URL, params=data).prepare()
+    return redirect(pr.url)
 
-    if request.method == 'GET':
-        print(request.args)
-    elif request.method == 'POST':
-        print(request.form)
-    else:
-        raise ValueError("I'm afraid I can't do that Dave")
-
+@app.route("/create", methods=['GET'])
+def create_post():
+    data = {'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+            'code': request.args['code']}
+    res = requests.post(GITHUB_TOKEN_URL, data=data)
+    token = parse_qs(res.text)['access_token'][0]
+    return token
     gh = Github(token)
+    return str(request.args)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port)
     app.run(debug=True, port=port)
